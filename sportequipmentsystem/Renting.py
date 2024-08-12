6e881e7b1aa27f8151bb7882e73c69baf0030f05
manoj <jmanojjmvr49@gmail.com>
2024-08-12 23:37:56 +0530

    first commit

---
+++ sportequipmentsystem/Renting.py
@@ -0,0 +1,58 @@
+import datetime as dt
+# Write initial data to the file
+# equipment rentening tracking
+# Function to write initial data to the file
+def update_barrow():
+    try:
+        with open('rentaldatabase.txt','r')as ch:
+            data = ch.read().strip().split('|')
+            usn = input("Enter USN: ")
+            for i in data:
+                if usn not in i:
+                    with open("rentaldatabase.txt", 'a') as fs:  
+                        eid = input("Enter the equipment ID: ")
+                        name = input("Enter name: ")
+                        time_barrow = dt.datetime.now().time() # we can change the time() to date() also
+                        s = f"{usn}|{eid}|{name}|{time_barrow}"
+                        fs.write(s)
+                        print("Barrow sucessfull !")
+                else:
+                    print("usn is already exist ")
+                    return 
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Function to update the name based on eid
+def update_return():
+    try:
+
+         with open('rentaldatabase.txt','r')as ch:
+            data = ch.read().strip().split('|')
+            eid = input("Enter the equipment ID: ")
+            for i in data:
+                if eid in i:               
+                    with open('rentaldatabase.txt', 'r') as fn:
+                        lines = fn.readlines()  # Read all lines
+                    #f"{usn}|{eid}|{name}|{time_barrow}" for position check                 
+                    updated_lines = []
+                    for line in lines:
+                        parts = line.strip().split('|')
+                        if parts[1] == eid:
+                            parts[4] = str(dt.datetime.now().time()) # Update the name
+                        updated_lines.append("|".join(parts) + '\n')
+
+                    with open('rentaldatabase.txt', 'w') as fn:
+                        fn.writelines(updated_lines)  # Write updated lines back to the file
+                else:
+                    print("eid is not exist :")
+                    return
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+
+
+            
+
+
+
---
+++ sportequipmentsystem/sportmenu.py
@@ -0,0 +1,67 @@
+from Renting import *
+from sportdatabase import *
+
+class Sports:
+  def __init__(self) -> None:
+    pass
+
+  def sportEquipmentDetails(self):
+    print("\nEquipment Details")
+    while True:
+        print("\nSport Database ")
+        print("1.entryEquipment\n2.readEquipment\n3.delete(defected)\n4.update_status\n5.exit\nchoice : ")
+        inpu = int(input())
+        match(inpu):
+          case 1:appendcontent()
+          case 2:display()  
+          case 3:delete()
+          case 4:update_status()
+          case 5 :break
+          # case _ : print("invalid choice : press [cntrl + c] to exit or try-again :")
+          
+
+  def sportEquipmentRenting(self):
+    while True:
+      print("Renting tracking")
+      print("1.barrow 2.return 3.exit")
+      inpu = int(input())
+      match(inpu):
+        case 1:update_barrow()  
+        case 2:update_return()
+        case 3 : break 
+
+  def sportEquipmentTracking(self):
+    try:
+      print("\nSport Equipment Details")
+      print("-------------------------------------------------------")
+      with open('sportequipment.txt','r') as fs:
+        data = fs.read()
+        # l =[str(ele) for ele in data] #to print in the form of string not in list 
+        # dis = ' |'.join(l)
+        print(data)
+
+      print("Sport Equipment Tracking")
+      print("-------------------------------------------------------")
+      with open('rentaldatabase.txt','r') as f:
+        data = f.readlines()
+        print("usn|eid|name|time_barrowed|time_returned")
+        l = [str(ele) for ele in  data]
+        dis = ' |'.join(l)
+        print(dis)
+
+      
+    except Exception as error :
+      print(error)
+    return
+        
+obj = Sports()
+
+while True:
+  print("Sport Equipment System")  
+  ch = int(input("1.sport Equipment details\n2.sport Equipment Renting\n3.sport Equipment Tracking\n4.Exit\nenter the choice :"))
+  match(ch):
+    case 1: obj.sportEquipmentDetails()
+    case 2: obj.sportEquipmentRenting()
+    case 3: obj.sportEquipmentTracking()
+    case 4: break
+
